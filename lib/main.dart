c0f9aa2a3be9cbdaa8b15d6f420d2a27ddda7776
ahmedhlulu <ahmedhlulu@icloud.com>
2023-06-10 02:08:50 +0300

    Add Animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
 
@@ -23,8 +24,22 @@ class _GameState extends State<Game> {
   int score = 0;
   int games = 0;
 
+  double animatedScaleTop = 0;
+
+  double animatedTurnsTop = 0;
+  Duration animatedTurnsDurationTop = const Duration(seconds: 2);
+
+  double animatedScale = 1;
+  Duration animatedScaleDuration = const Duration(seconds: 0);
+  Duration animatedScaleDurationTimer = const Duration(seconds: 1);
+
+
   void play() {
     setState(() {
+      animatedScale = 0;
+      animatedScaleTop = 1;
+      animatedTurnsTop += 4;
+
       leftPic = Random().nextInt(3) + 1;
       rightPic = Random().nextInt(3) + 1;
       if (leftPic == rightPic) {
@@ -36,22 +51,30 @@ class _GameState extends State<Game> {
       }
     });
 
-    bool winner = true;
-    if (leftPic == 1 && rightPic == 2) {
-      winner = false;
-    } else if (leftPic == 1 && rightPic == 3) {
-      winner = true;
-    } else if (leftPic == 2 && rightPic == 1) {
-      winner = true;
-    } else if (leftPic == 2 && rightPic == 3) {
-      winner = false;
-    } else if (leftPic == 3 && rightPic == 1) {
-      winner = false;
-    } else {
-      winner = true;
-    }
-    score += winner ? 1 : 0;
+    Timer(animatedScaleDurationTimer, () {
+      setState(() {
+        animatedScale = 1;
+        animatedScaleTop = 0;
+      });
+    });
+
+    score += winner() ? 1 : 0;
     games++;
+  }
+
+  bool winner(){
+    if (leftPic == 1 && rightPic == 2) {
+      return false;
+    } else if (leftPic == 1 && rightPic == 3) {
+      return true;
+    } else if (leftPic == 2 && rightPic == 1) {
+      return true;
+    } else if (leftPic == 2 && rightPic == 3) {
+      return false;
+    } else if (leftPic == 3 && rightPic == 1) {
+      return false;
+    }
+    return true;
   }
 
   @override
@@ -64,59 +87,98 @@ class _GameState extends State<Game> {
           backgroundColor: Colors.blue.shade900,
           centerTitle: true,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Stack(
           children: [
-            Text(
-              "game no: $games",
-              style: const TextStyle(fontSize: 24, color: Colors.white),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: TextButton(
-                    onPressed: play,
+            Container(
+              margin: const EdgeInsets.only(top: 24),
+              height: 100,
+              child: AnimatedRotation(
+                turns: animatedTurnsTop,
+                duration: animatedTurnsDurationTop,
+                child: AnimatedScale(
+                  scale: animatedScaleTop,
+                  duration: animatedScaleDuration,
+                  child: Expanded(
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Image.asset("images/hand$leftPic.png"),
-                        Text(
-                          "You",
-                          style: TextStyle(fontSize: 24, color: score > games - score ? Colors.greenAccent : Colors.pink),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset("images/hand1.png",height: 50,width: 50),
+                            const SizedBox(width: 15),
+                            Image.asset("images/hand2.png",height: 50,width: 50),
+                          ],
                         ),
-                        Text(
-                          "$score",
-                          style: TextStyle(fontSize: 24, color: score > games - score ? Colors.greenAccent : Colors.pink),
-                        ),
+                        Image.asset("images/hand3.png",height: 50,width: 50)
                       ],
                     ),
                   ),
                 ),
-                const Text(
-                  "VS",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset("images/hand$rightPic.png"),
-                        Text(
-                          "System",
-                          style: TextStyle(fontSize: 24, color: score < games - score ? Colors.greenAccent : Colors.pink),
-                        ),
-                        Text(
-                          "${games - score}",
-                          style: TextStyle(fontSize: 24, color: score < games - score ? Colors.greenAccent : Colors.pink),
-                        ),
-                      ],
-                    ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AnimatedScale(
+                  scale: animatedScale,
+                  duration: animatedScaleDuration,
+                  child: Column(
+                    children: [
+                      Text(
+                        winner() ? "You Win 🥳🥳" : "You Los",
+                        style: TextStyle(fontSize: 24, color: winner() ? Colors.greenAccent : Colors.redAccent),
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextButton(
+                              onPressed: play,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset("images/hand$leftPic.png"),
+                                  Text(
+                                    "You",
+                                    style: TextStyle(fontSize: 24, color: winner() ? Colors.greenAccent : Colors.pink),
+                                  ),
+                                  // Text(
+                                  //   "$score",
+                                  //   style: TextStyle(fontSize: 24, color: score > games - score ? Colors.greenAccent : Colors.pink),
+                                  // ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const Text(
+                            "VS",
+                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset("images/hand$rightPic.png"),
+                                  Text(
+                                    "System",
+                                    style: TextStyle(fontSize: 24, color: !winner() ? Colors.greenAccent : Colors.pink),
+                                  ),
+                                  // Text(
+                                  //   "${games - score}",
+                                  //   style: TextStyle(fontSize: 24, color: score < games - score ? Colors.greenAccent : Colors.pink),
+                                  // ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ],
-            ),
+            )
           ],
         ),
       ),
